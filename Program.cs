2fef4ec887d53706651c3568d7cfc387d480bdda
Brandon Hill <bhilldev@outlook.com>
2026-01-16 10:19:05 -0600

    Registered DbContext in Program.cs

--- Program.cs
+++ Program.cs
@@ -1,17 +1,28 @@
+using Microsoft.EntityFrameworkCore;
+using MyEcommerceApp.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+// Register DbContext
+builder.Services.AddDbContext<EcommerceDbContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// Add services to the container
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Configure the HTTP request pipeline
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
 
 app.Run();
